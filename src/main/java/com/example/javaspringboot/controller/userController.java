92b7f6ca9897f5ccc141512d7c3a818c3c437dcf
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2023-02-04 16:46:37 +0700

    insert user

--- src/main/java/com/example/javaspringboot/controller/userController.java
+++ src/main/java/com/example/javaspringboot/controller/userController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.javaspringboot.mapper.UsersMapper;
@@ -13,13 +16,14 @@ import com.example.javaspringboot.model.UsersExample;
 import com.google.gson.Gson;
 
 @RestController
+@RequestMapping("/api")
 public class userController {
 
 	@Autowired
 	UsersMapper userMapper;
 	
 	@GetMapping("/users")
-	public String hello() {
+	public String GetAllUsers() {
 		
 		UsersExample example  = new UsersExample();
 		List<Users> list = userMapper.selectByExample(example);
@@ -30,5 +34,11 @@ public class userController {
 		String json = new Gson().toJson(list);
 		
 		return json;
+	}
+	
+	@PostMapping("/users/signup")
+	public Users SignUp(@RequestBody Users user) {
+		userMapper.insert(user);
+		return user;
 	}
 }
